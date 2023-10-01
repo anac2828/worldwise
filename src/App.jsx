169b35f01fd22fe47d17401020e07f3aa127d96a
Ana MacBook <anac2828@yahoo.com>
2023-10-01 15:03:27 -0700

    set up the rest of the components

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <h1>Hellow Router!</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='product' element={<Product />} />
+          <Route index element={<Homepage />} />
           <Route path='pricing' element={<Pricing />} />
-          <Route path='app' element={<AppLayout />} />
+          <Route path='product' element={<Product />} />
+          <Route path='login' element={<Login />} />
+          {/* APP ROUTE */}
+          <Route path='app' element={<AppLayout />}>
+            {/* Default element that will be displayed on the app page */}
+            <Route index element={<p>List of cites</p>} />
+            {/* NESTED ROUTES (CHIELD ROUTES) */}
+            <Route path='cities' element={<p>List of cities</p>} />
+            <Route path='countries' element={<p>Counties</p>} />
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
+          {/*  */}
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>

1a9fabebb408c230b457704254f4809f043ef514
anac2828 <anac2828@yahoo.com>
2023-10-05 16:32:04 -0700

    finished login in and logout

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,46 @@ import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
+
 import { CitiesProvider } from './context/CitiesContext';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <div>
-      <CitiesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path='pricing' element={<Pricing />} />
-            <Route path='product' element={<Product />} />
-            <Route path='login' element={<Login />} />
-            {/* APP ROUTE */}
-            <Route path='app' element={<AppLayout />}>
-              {/* NESTED ROUTES (CHIELD ROUTES) */}
-              {/* Default element that will be displayed on the app page */}
-              {/* The navigate component acts as a "redirect", "replace" will take you back to the previous screen when the back arrow in the browser is click*/}
-              <Route index element={<Navigate replace to='cities' />} />
-              <Route path='cities' element={<CityList />} />
-              {/* This route will display details about a specific city when it is clicked from the cities list */}
-              <Route path='cities/:id' element={<City />} />
-              <Route path='countries' element={<CountryList />} />
-              <Route path='form' element={<Form />} />
-            </Route>
-            {/*  */}
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </CitiesProvider>
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path='pricing' element={<Pricing />} />
+              <Route path='product' element={<Product />} />
+
+              <Route path='login' element={<Login />} />
+
+              {/* APP ROUTE */}
+              <Route path='app' element={<AppLayout />}>
+                {/* NESTED ROUTES (CHIELD ROUTES) */}
+                {/* Default element that will be displayed on the app page */}
+                {/* The navigate component acts as a "redirect", "replace" will take you back to the previous screen when the back arrow in the browser is click*/}
+                <Route index element={<Navigate replace to='cities' />} />
+                <Route path='cities' element={<CityList />} />
+                {/* This route will display details about a specific city when it is clicked from the cities list */}
+                <Route path='cities/:id' element={<City />} />
+                <Route path='countries' element={<CountryList />} />
+                <Route path='form' element={<Form />} />
+              </Route>
+              {/*  */}
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,35 @@
-import styles from './Login.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../context/AuthContext';
+
 import PageNav from '../components/PageNav';
+import styles from './Login.module.css';
+import Button from '../components/Button';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const { login, isAuthenticated } = useAuth();
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com');
   const [password, setPassword] = useState('qwerty');
 
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
+  useEffect(() => {
+    // use "replace: true"
+    if (isAuthenticated) navigate('/app', { replace: true });
+  }, [isAuthenticated, navigate]);
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleOnSubmit}>
         <div className={styles.row}>
           <label htmlFor='email'>Email address</label>
           <input
@@ -32,7 +51,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type={'primary'}> Login</Button>
         </div>
       </form>
     </main>
